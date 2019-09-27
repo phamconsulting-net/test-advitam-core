f46bccfb9426576d5b9b74180854df22c3c5b58f
Emmaunuel CASTRO <Emmanuel CASTRO emmanuel.castro@capgemini.com>
2019-09-27 11:42:47 +0200

    US-002 Ajout d'une fonctionalité inutile

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -3,6 +3,7 @@ package test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
+        System.out.println("2+2="+(UselessTools.add(2,2)));
     }
 }
 
---
+++ test/test/UselessToolsTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UselessToolsTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        assertEquals(2, UselessTools.add(1,1));
+    }
+}
