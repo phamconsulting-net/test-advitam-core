231f36920b62eaf241816e02eb229084fa66c670
Emmaunuel CASTRO <Emmanuel CASTRO emmanuel.castro@capgemini.com>
2019-09-27 12:00:25 +0200

    UselessTools deleta est. Et vidit Deus InutilemFabrilia quod esset bona

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -3,7 +3,7 @@ package test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
-        System.out.println("2+2="+(UselessTools.add(2,2)));
+        System.out.println("2+2="+(InutilemFabrilia.add(2,2)));
     }
 }
 
